30db8a12882e1437760196bb3479218b24a9d00d
zachsg <zach@bindl.co>
2022-01-24 08:59:01 -0800

    refactor settings

--- lib/screens/settings/display_name_widget.dart
+++ lib/screens/settings/display_name_widget.dart
@@ -38,51 +38,55 @@ class _DisplayNameWidgetState extends ConsumerState<DisplayNameWidget> {
       padding: const EdgeInsets.symmetric(horizontal: 12.0),
       child: Row(
         children: [
-          Expanded(
-            child: TextFormField(
-              controller: _textController,
-              textInputAction: TextInputAction.done,
-              minLines: 1,
-              maxLines: 6,
-              textCapitalization: TextCapitalization.sentences,
-              style: Theme.of(context).textTheme.bodyText2,
-              decoration: const InputDecoration(
-                isDense: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(16.0),
-                  ),
-                ),
-                labelText: displayNameLabel,
-              ),
-            ),
-          ),
-          IconButton(
-            onPressed: () async {
-              if (_textController.text.isNotEmpty) {
-                var displayName = _textController.text.trim();
-
-                if (displayName.isNotEmpty) {
-                  await ref.read(userProvider).updateDisplayName(displayName);
-
-                  _textController.clear();
-
-                  FocusScope.of(context).unfocus();
-
-                  final snackBar = SnackBar(
-                    content: Text('$displayName $updatedLabel'),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-              }
-            },
-            icon: Icon(
-              Icons.task_alt,
-              size: 32,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
+          Expanded(child: _displayNameTextField(context)),
+          _saveButton(context),
         ],
+      ),
+    );
+  }
+
+  IconButton _saveButton(BuildContext context) {
+    return IconButton(
+      onPressed: () async {
+        if (_textController.text.isNotEmpty) {
+          var displayName = _textController.text.trim();
+
+          if (displayName.isNotEmpty) {
+            await ref.read(userProvider).updateDisplayName(displayName);
+
+            _textController.clear();
+
+            FocusScope.of(context).unfocus();
+
+            final snackBar = SnackBar(
+              content: Text('$displayName $updatedLabel'),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          }
+        }
+      },
+      icon: Icon(
+        Icons.task_alt,
+        size: 32,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+    );
+  }
+
+  TextFormField _displayNameTextField(BuildContext context) {
+    return TextFormField(
+      controller: _textController,
+      textInputAction: TextInputAction.done,
+      minLines: 1,
+      maxLines: 6,
+      textCapitalization: TextCapitalization.sentences,
+      style: Theme.of(context).textTheme.bodyText2,
+      decoration: const InputDecoration(
+        isDense: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+        ),
+        labelText: displayNameLabel,
       ),
     );
   }
--- lib/screens/settings/settings_view.dart
+++ lib/screens/settings/settings_view.dart
@@ -1,4 +1,5 @@
 import 'package:bodai/controllers/providers.dart';
+import 'package:bodai/controllers/user_controller.dart';
 import 'package:bodai/screens/sign_in/sign_in_view.dart';
 import 'package:bodai/shared_widgets/xwidgets.dart';
 import 'package:bodai/utils/strings.dart';
@@ -28,166 +29,51 @@ class SettingsView extends ConsumerWidget {
               const SizedBox(height: 16),
               const DisplayNameWidget(),
               const SizedBox(height: 16),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        lookingForLabel,
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                      DropdownButton(
-                        elevation: 4,
-                        borderRadius: BorderRadius.circular(10),
-                        value: up.servings - 1,
-                        icon: const SizedBox(),
-                        underline: const SizedBox(),
-                        items: _getServingsOptions(context),
-                        onChanged: (value) async {
-                          var servings = (value as int) + 1;
-
-                          await ref.read(userProvider).setServings(servings);
-                        },
-                      ),
-                      Text(
-                        up.servings == 1 ? '$personLabel.' : '$peopleLabel.',
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              _servingsCard(context, up, ref),
               const SizedBox(height: 16),
-              // Card(
-              //   child: Padding(
-              //     padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              //     child: Row(
-              //       children: [
-              //         Text(
-              //           iWantLabel,
-              //           style: Theme.of(context).textTheme.headline2,
-              //         ),
-              //         DropdownButton(
-              //           elevation: 4,
-              //           borderRadius: BorderRadius.circular(10),
-              //           value: up.numMeals - 1,
-              //           icon: const SizedBox(),
-              //           underline: const SizedBox(),
-              //           items: _getNumMealsOptions(context),
-              //           onChanged: (value) async {
-              //             var meals = (value as int) + 1;
-
-              //             await ref.read(userProvider).setNumMeals(meals);
-              //             // await ref.read(userProvider).computeMealPlan();
-              //             ref.read(bestMealProvider.notifier).compute();
-              //             await ref.read(pantryProvider).clear();
-              //             ref.read(mealPlanProvider).load();
-              //           },
-              //         ),
-              //         Text(
-              //           up.numMeals == 1 ? '$mealLabel.' : '$mealsLabel.',
-              //           style: Theme.of(context).textTheme.headline2,
-              //         ),
-              //       ],
-              //     ),
-              //   ),
-              // ),
-              // const SizedBox(height: 16),
               const AllergyCard(shouldPersist: true),
               const SizedBox(height: 16),
               const AdoreIngredientsCard(shouldPersist: true),
               const SizedBox(height: 16),
               const AbhorIngredientsCard(shouldPersist: true),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: DropdownButton<ThemeMode>(
-                      elevation: 4,
-                      borderRadius: BorderRadius.circular(10),
-                      icon: Icon(
-                        Icons.color_lens_outlined,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                      underline: const SizedBox(),
-                      value: ref.watch(settingsProvider).themeMode,
-                      onChanged:
-                          ref.watch(settingsProvider.notifier).updateThemeMode,
-                      items: [
-                        DropdownMenuItem(
-                          value: ThemeMode.system,
-                          child: Text(
-                            systemThemeLabel,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText2
-                                ?.copyWith(
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                    fontWeight: FontWeight.w600),
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: ThemeMode.light,
-                          child: Text(
-                            lightThemeLabel,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText2
-                                ?.copyWith(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: ThemeMode.dark,
-                          child: Text(
-                            darkThemeLabel,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText2
-                                ?.copyWith(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text(
-                      '$versionLabel: ${ref.watch(settingsProvider).packageInfo.version}'
-                      ' (${ref.watch(settingsProvider).packageInfo.buildNumber})',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText2
-                          ?.copyWith(color: Colors.grey),
-                    ),
-                  ),
-                ],
-              ),
-              TextButton(
-                onPressed: () async {
-                  await ref.read(settingsProvider.notifier).signOut();
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, SignInView.routeName, (route) => false);
-                },
-                child: Text(
-                  'LOGOUT',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText2
-                      ?.copyWith(color: Theme.of(context).colorScheme.primary),
-                ),
-              ),
+              _themeButton(context, ref),
+              _signOutButton(ref, context),
               const SizedBox(height: 16),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Card _servingsCard(BuildContext context, UserController up, WidgetRef ref) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          children: [
+            Text(
+              lookingForLabel,
+              style: Theme.of(context).textTheme.headline2,
+            ),
+            DropdownButton(
+              elevation: 4,
+              borderRadius: BorderRadius.circular(10),
+              value: up.servings - 1,
+              icon: const SizedBox(),
+              underline: const SizedBox(),
+              items: _getServingsOptions(context),
+              onChanged: (value) async {
+                var servings = (value as int) + 1;
+
+                await ref.read(userProvider).setServings(servings);
+              },
+            ),
+            Text(
+              up.servings == 1 ? '$personLabel.' : '$peopleLabel.',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          ],
         ),
       ),
     );
@@ -214,5 +100,86 @@ class SettingsView extends ConsumerWidget {
     }
 
     return items;
+  }
+
+  Row _themeButton(BuildContext context, WidgetRef ref) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: DropdownButton<ThemeMode>(
+            elevation: 4,
+            borderRadius: BorderRadius.circular(10),
+            icon: Icon(
+              Icons.color_lens_outlined,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            underline: const SizedBox(),
+            value: ref.watch(settingsProvider).themeMode,
+            onChanged: ref.watch(settingsProvider.notifier).updateThemeMode,
+            items: [
+              DropdownMenuItem(
+                value: ThemeMode.system,
+                child: Text(
+                  systemThemeLabel,
+                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.light,
+                child: Text(
+                  lightThemeLabel,
+                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.dark,
+                child: Text(
+                  darkThemeLabel,
+                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            '$versionLabel: ${ref.watch(settingsProvider).packageInfo.version}'
+            ' (${ref.watch(settingsProvider).packageInfo.buildNumber})',
+            style: Theme.of(context)
+                .textTheme
+                .bodyText2
+                ?.copyWith(color: Colors.grey),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TextButton _signOutButton(WidgetRef ref, BuildContext context) {
+    return TextButton(
+      onPressed: () async {
+        await ref.read(settingsProvider.notifier).signOut();
+        Navigator.pushNamedAndRemoveUntil(
+            context, SignInView.routeName, (route) => false);
+      },
+      child: Text(
+        'LOGOUT',
+        style: Theme.of(context)
+            .textTheme
+            .bodyText2
+            ?.copyWith(color: Theme.of(context).colorScheme.primary),
+      ),
+    );
   }
 }
