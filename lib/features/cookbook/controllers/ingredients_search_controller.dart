5ef10ea66abe947e74c631fd0b7cb428e49c65a0
zachsg <zach@bindl.co>
2022-02-10 12:29:54 -0700

    cleanup ingredient search widget

--- lib/features/cookbook/controllers/ingredients_search_controller.dart
+++ lib/features/cookbook/controllers/ingredients_search_controller.dart
@@ -1,5 +1,8 @@
+import 'package:bodai/utils/helpers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../shared_controllers/providers.dart';
+import '../../../shared_models/xmodels.dart';
 import 'cookbook_controller.dart';
 
 final ingredientsSearchProvider =
@@ -19,6 +22,9 @@ class IngredientsSearchController extends StateNotifier<List<String>> {
     state = [...state, ingredient.toLowerCase().trim()];
 
     ref.read(cookbookProvider.notifier).load();
+    ref.read(bestMealProvider.notifier).compute();
+
+    Ingredients.allSimpleComplete.remove(ingredient);
   }
 
   void removeIngredientToUse(String ingredient) {
@@ -27,5 +33,8 @@ class IngredientsSearchController extends StateNotifier<List<String>> {
         .toList();
 
     ref.read(cookbookProvider.notifier).load();
+    ref.read(bestMealProvider.notifier).compute();
+
+    Ingredients.allSimpleComplete.add(ingredient.capitalize());
   }
 }
--- lib/features/cookbook/ingredient_filter_widget.dart
+++ lib/features/cookbook/ingredient_filter_widget.dart
@@ -1,6 +1,5 @@
 import 'package:bodai/features/cookbook/controllers/ingredients_search_controller.dart';
 import 'package:bodai/features/cookbook/controllers/sort_order_controller.dart';
-import 'package:bodai/shared_controllers/providers.dart';
 import 'package:bodai/shared_models/xmodels.dart';
 import 'package:bodai/utils/strings.dart';
 import 'package:flutter/material.dart';
@@ -38,102 +37,49 @@ class _IngredientFilterWidget extends ConsumerState<IngredientFilterWidget> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 4.0,
-                  bottom: 2.0,
-                  left: 4.0,
-                  right: 4.0,
-                ),
-                child: Wrap(
-                  alignment: WrapAlignment.center,
-                  spacing: 8,
-                  children: [
-                    ChoiceChip(
-                      label: const Text('Latest'),
-                      selected:
-                          ref.watch(sortOrderProvider) == SortOrder.latest,
-                      onSelected: (selected) {
-                        ref
-                            .read(sortOrderProvider.notifier)
-                            .sortMeals(SortOrder.latest);
-                      },
-                    ),
-                    ChoiceChip(
-                      label: const Text('Quick Bites'),
-                      selected:
-                          ref.watch(sortOrderProvider) == SortOrder.quickest,
-                      onSelected: (selected) {
-                        ref
-                            .read(sortOrderProvider.notifier)
-                            .sortMeals(SortOrder.quickest);
-                      },
-                    ),
-                    ChoiceChip(
-                      label: const Text('Fewer Ingredients'),
-                      selected:
-                          ref.watch(sortOrderProvider) == SortOrder.fewest,
-                      onSelected: (selected) {
-                        ref
-                            .read(sortOrderProvider.notifier)
-                            .sortMeals(SortOrder.fewest);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            TypeAheadFormField(
-              textFieldConfiguration: TextFieldConfiguration(
-                controller: _textController,
-                scrollPadding: const EdgeInsets.only(bottom: 200),
-                decoration: const InputDecoration(
-                  isDense: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(16.0),
-                    ),
-                  ),
-                  labelText: 'Filter by ingredients you want to use',
-                ),
-              ),
-              suggestionsCallback: (pattern) {
-                return Ingredients.getSuggestions(
-                  ref: ref,
-                  pattern: pattern,
-                  inCookbook: true,
-                );
-              },
-              itemBuilder: (context, suggestion) {
-                return ListTile(
-                  title: Text(suggestion as String),
-                );
-              },
-              transitionBuilder: (context, suggestionsBox, controller) {
-                return suggestionsBox;
-              },
-              onSuggestionSelected: (ingredient) {
-                _textController.clear();
+            _filtersWrap(),
+            _ingredientSearchField(),
+            _ingredientsWrap(),
+          ],
+        ),
+      ),
+    );
+  }
 
+  Center _filtersWrap() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 4, bottom: 2, left: 4, right: 4),
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 8,
+          children: [
+            ChoiceChip(
+              label: const Text('Latest'),
+              selected: ref.watch(sortOrderProvider) == SortOrder.latest,
+              onSelected: (selected) {
                 ref
-                    .read(ingredientsSearchProvider.notifier)
-                    .setIngredientToUse(ingredient as String);
-
-                Ingredients.allSimpleComplete.remove(ingredient);
-
-                ref.read(bestMealProvider.notifier).compute();
+                    .read(sortOrderProvider.notifier)
+                    .sortMeals(SortOrder.latest);
               },
-              validator: (value) {
-                if (value != null && value.isEmpty) {
-                  return typeIngredientLabel;
-                }
-              },
-              onSaved: (value) {},
             ),
-            Wrap(
-              spacing: 12,
-              children: buildIngredientChips(),
+            ChoiceChip(
+              label: const Text('Quick Bites'),
+              selected: ref.watch(sortOrderProvider) == SortOrder.quickest,
+              onSelected: (selected) {
+                ref
+                    .read(sortOrderProvider.notifier)
+                    .sortMeals(SortOrder.quickest);
+              },
+            ),
+            ChoiceChip(
+              label: const Text('Fewer Ingredients'),
+              selected: ref.watch(sortOrderProvider) == SortOrder.fewest,
+              onSelected: (selected) {
+                ref
+                    .read(sortOrderProvider.notifier)
+                    .sortMeals(SortOrder.fewest);
+              },
             ),
           ],
         ),
@@ -141,7 +87,49 @@ class _IngredientFilterWidget extends ConsumerState<IngredientFilterWidget> {
     );
   }
 
-  List<Widget> buildIngredientChips() {
+  TypeAheadFormField<String> _ingredientSearchField() {
+    return TypeAheadFormField(
+      textFieldConfiguration: TextFieldConfiguration(
+        controller: _textController,
+        scrollPadding: const EdgeInsets.only(bottom: 200),
+        decoration: const InputDecoration(
+          isDense: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+          labelText: 'Filter by ingredients you want to use',
+        ),
+      ),
+      suggestionsCallback: (pattern) {
+        return Ingredients.getSuggestions(
+          ref: ref,
+          pattern: pattern,
+          inCookbook: true,
+        );
+      },
+      itemBuilder: (context, suggestion) {
+        return ListTile(title: Text(suggestion));
+      },
+      transitionBuilder: (context, suggestionsBox, controller) {
+        return suggestionsBox;
+      },
+      onSuggestionSelected: (ingredient) {
+        _textController.clear();
+
+        ref
+            .read(ingredientsSearchProvider.notifier)
+            .setIngredientToUse(ingredient);
+      },
+      validator: (value) {
+        return value != null && value.isEmpty ? typeIngredientLabel : null;
+      },
+      onSaved: (value) {},
+    );
+  }
+
+  Widget _ingredientsWrap() {
     List<Widget> chips = [];
 
     for (var ingredient in ref.watch(ingredientsSearchProvider)) {
@@ -152,15 +140,14 @@ class _IngredientFilterWidget extends ConsumerState<IngredientFilterWidget> {
           ref
               .read(ingredientsSearchProvider.notifier)
               .removeIngredientToUse(ingredient);
-
-          ref.read(bestMealProvider.notifier).compute();
-
-          Ingredients.allSimpleComplete.add(ingredient);
         },
       );
       chips.add(chip);
     }
 
-    return chips;
+    return Wrap(
+      spacing: 12,
+      children: chips,
+    );
   }
 }
