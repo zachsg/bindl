92961254122efff39bac00c3a35b6136e54dabda
zachsg <zach@bindl.co>
2022-03-24 18:37:33 -0700

    better ux for recipe cards in your and my profile views

--- lib/features/profile/my_recipes/widgets/my_recipes_widget.dart
+++ lib/features/profile/my_recipes/widgets/my_recipes_widget.dart
@@ -1,3 +1,4 @@
+import 'package:bodai/extensions.dart';
 import 'package:bodai/features/profile/my_recipes/edit_recipe_view.dart';
 import 'package:bodai/features/profile/my_recipes/my_recipes_controller.dart';
 import 'package:flutter/material.dart';
@@ -19,22 +20,76 @@ class MyRecipesWidget extends HookConsumerWidget {
             restorationId: 'myRecipesListView',
             itemCount: ref.watch(myRecipesProvider).length,
             itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                child: GestureDetector(
-                  onTap: () {
-                    ref
-                        .read(editRecipeProvider.notifier)
-                        .setup(ref.read(myRecipesProvider)[index]);
+              final recipe = ref.watch(myRecipesProvider)[index];
 
-                    Navigator.pushNamed(context, EditRecipeView.routeName);
-                  },
-                  child: Card(
-                      child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(ref.watch(myRecipesProvider)[index].name),
-                  )),
-                ),
+              return Column(
+                children: [
+                  Container(
+                    height: 1,
+                    color:
+                        Theme.of(context).colorScheme.primary.withOpacity(0.3),
+                  ),
+                  ListTile(
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                    title: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(4.0)),
+                          child: Image.network(
+                            recipe.imageUrl,
+                            fit: BoxFit.cover,
+                            height: 64,
+                            width: 64,
+                          ),
+                        ),
+                        const SizedBox(width: 8.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              recipe.name,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 4.0),
+                              child: Row(
+                                children: [
+                                  const Icon(Icons.schedule, size: 18),
+                                  const SizedBox(width: 4),
+                                  Text(
+                                      '${recipe.prepTime + recipe.cookTime} min'),
+                                  const SizedBox(width: 16),
+                                  const Icon(Icons.restaurant, size: 18),
+                                  const SizedBox(width: 4),
+                                  Text(
+                                      '${recipe.ingredients.length} ingredients'),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Text('#${recipe.diet.name.capitalize()}'),
+                                const SizedBox(width: 16),
+                                Text('#${recipe.cuisine.name.capitalize()}'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      ref.read(editRecipeProvider.notifier).setup(recipe);
+
+                      Navigator.pushNamed(context, EditRecipeView.routeName);
+                    },
+                  ),
+                ],
               );
             },
           )
--- lib/features/profile/your_recipes/your_recipes_widget.dart
+++ lib/features/profile/your_recipes/your_recipes_widget.dart
@@ -1,3 +1,4 @@
+import 'package:bodai/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -20,22 +21,89 @@ class YourRecipesWidget extends HookConsumerWidget {
             itemCount: ref.watch(yourRecipesProvider).length,
             itemBuilder: (BuildContext context, int index) {
               //TODO: Replace with Recipe card probably for nicer UI
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                child: GestureDetector(
-                  onTap: () {
-                    ref
-                        .read(recipeProvider.notifier)
-                        .setupSelf(ref.read(yourRecipesProvider)[index].id!);
+              final recipe = ref.watch(yourRecipesProvider)[index];
+              final mutualIngredients = ref
+                  .read(recipeProvider.notifier)
+                  .crossReferencedRecipeIngredientsWithPantry(recipe);
 
-                    Navigator.pushNamed(context, RecipeView.routeName);
-                  },
-                  child: Card(
-                      child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(ref.watch(yourRecipesProvider)[index].name),
-                  )),
-                ),
+              return Column(
+                children: [
+                  Container(
+                    height: 1,
+                    color:
+                        Theme.of(context).colorScheme.primary.withOpacity(0.3),
+                  ),
+                  ListTile(
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                    title: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(4.0)),
+                          child: Image.network(
+                            recipe.imageUrl,
+                            fit: BoxFit.cover,
+                            height: 64,
+                            width: 64,
+                          ),
+                        ),
+                        const SizedBox(width: 8.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              recipe.name,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 4.0),
+                              child: Row(
+                                children: [
+                                  const Icon(Icons.schedule, size: 18),
+                                  const SizedBox(width: 4),
+                                  Text(
+                                      '${recipe.prepTime + recipe.cookTime} min'),
+                                  const SizedBox(width: 16),
+                                  const Icon(Icons.restaurant, size: 18),
+                                  const SizedBox(width: 4),
+                                  Text(
+                                      '${recipe.ingredients.length} ingredients'),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Text('#${recipe.diet.name.capitalize()}'),
+                                const SizedBox(width: 16),
+                                Text('#${recipe.cuisine.name.capitalize()}'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    subtitle: mutualIngredients.isNotEmpty
+                        ? Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 6.0),
+                            child: Text(
+                              'Uses your: $mutualIngredients',
+                              style:
+                                  const TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          )
+                        : const SizedBox(),
+                    onTap: () {
+                      ref.read(recipeProvider.notifier).setupSelf(recipe.id!);
+
+                      Navigator.pushNamed(context, RecipeView.routeName);
+                    },
+                  ),
+                ],
               );
             },
           )
