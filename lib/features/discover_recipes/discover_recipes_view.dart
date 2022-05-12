236e135a89216b3992f9946ec069f2f01e95c1a2
zachsg <zach@bindl.co>
2022-05-12 12:28:23 -0700

    comment out expansion panel to filter on expiring ingredients until finished

--- lib/features/discover_recipes/discover_recipes_view.dart
+++ lib/features/discover_recipes/discover_recipes_view.dart
@@ -114,47 +114,47 @@ class DiscoverRecipesView extends ConsumerWidget {
                 width: double.infinity,
                 color: Theme.of(context).colorScheme.primary.withOpacity(0.5),
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 10.0,
-                  right: 10.0,
-                  top: 8.0,
-                ),
-                child: ExpansionPanelList(
-                  children: [
-                    ExpansionPanel(
-                      headerBuilder: (context, isExpanded) {
-                        return const Padding(
-                          padding: EdgeInsets.only(top: 8.0, left: 8.0),
-                          child: Text(
-                              'You have ingredients that are expiring soon! Filter recipes by them.'),
-                        );
-                      },
-                      body: Wrap(
-                        spacing: 8,
-                        children: [
-                          FilterChip(
-                            label: Text('Spinach'),
-                            selected: true,
-                            onSelected: (selected) {},
-                          ),
-                          FilterChip(
-                            label: Text('Cucumber'),
-                            onSelected: (selected) {},
-                          ),
-                        ],
-                      ),
-                      isExpanded:
-                          ref.watch(ingredientsExpiringSoonExpandedProvider),
-                    ),
-                  ],
-                  expansionCallback: (i, isExpanded) {
-                    ref
-                        .read(ingredientsExpiringSoonExpandedProvider.notifier)
-                        .state = !isExpanded;
-                  },
-                ),
-              ),
+              // Padding(
+              //   padding: const EdgeInsets.only(
+              //     left: 10.0,
+              //     right: 10.0,
+              //     top: 8.0,
+              //   ),
+              //   child: ExpansionPanelList(
+              //     children: [
+              //       ExpansionPanel(
+              //         headerBuilder: (context, isExpanded) {
+              //           return const Padding(
+              //             padding: EdgeInsets.only(top: 8.0, left: 8.0),
+              //             child: Text(
+              //                 'You have ingredients that are expiring soon! Filter recipes by them.'),
+              //           );
+              //         },
+              //         body: Wrap(
+              //           spacing: 8,
+              //           children: [
+              //             FilterChip(
+              //               label: Text('Spinach'),
+              //               selected: true,
+              //               onSelected: (selected) {},
+              //             ),
+              //             FilterChip(
+              //               label: Text('Cucumber'),
+              //               onSelected: (selected) {},
+              //             ),
+              //           ],
+              //         ),
+              //         isExpanded:
+              //             ref.watch(ingredientsExpiringSoonExpandedProvider),
+              //       ),
+              //     ],
+              //     expansionCallback: (i, isExpanded) {
+              //       ref
+              //           .read(ingredientsExpiringSoonExpandedProvider.notifier)
+              //           .state = !isExpanded;
+              //     },
+              //   ),
+              // ),
               const Expanded(
                 child: DiscoverRecipesListWidget(),
               ),
