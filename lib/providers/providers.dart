f5e283194941928ab91260dd807c286b0dc0849e
zachsg <zach@bindl.co>
2022-06-13 09:49:45 -0700

    import cleanup

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
 import '../models/xmodels.dart';
-import 'user_controller.dart';
 
 final loadingProvider = StateProvider<bool>((ref) => false);
 
