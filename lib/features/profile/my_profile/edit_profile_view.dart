aa7a2dadcc3b7288b57b3b52678a3660c74ab1b2
zachsg <zach@bindl.co>
2022-06-18 14:37:33 -0700

    add account deletion request feature

--- lib/features/profile/my_profile/edit_profile_view.dart
+++ lib/features/profile/my_profile/edit_profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -93,21 +94,84 @@ class EditProfileView extends ConsumerWidget {
                   ],
                 ),
                 const SizedBox(height: 16),
-                TextButton(
-                  onPressed: () async {
-                    final navigator = Navigator.of(context);
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      onPressed: () async {
+                        final navigator = Navigator.of(context);
 
-                    await AuthController.signOut();
-                    navigator.pushNamedAndRemoveUntil(
-                        AuthView.routeName, (route) => false);
-                  },
-                  child: const Text('Sign out'),
+                        await AuthController.signOut();
+                        navigator.pushNamedAndRemoveUntil(
+                            AuthView.routeName, (route) => false);
+                      },
+                      child: const Text('Sign out'),
+                    ),
+                    TextButton(
+                      onPressed: () => _showMyDialog(
+                          context,
+                          'Request Account Deletion',
+                          const Text(
+                              'Click \'Delete Me\' to confirm that you would like Bodai to remove your account. We\'ll email you a confirmation when it\'s done.'),
+                          ref),
+                      child: const Text('Delete account'),
+                    ),
+                  ],
                 ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Future<void> _showMyDialog(
+      BuildContext context, String title, Widget widget, WidgetRef ref) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(child: widget),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Delete Me'),
+              onPressed: () async {
+                final scaffoldMessenger = ScaffoldMessenger.of(context);
+                final navigator = Navigator.of(context);
+
+                if (supabase.auth.currentUser != null) {
+                  final Email email = Email(
+                    body:
+                        'Please delete my Bodai account associated with ${supabase.auth.currentUser?.email}',
+                    subject: 'Account Deletion Request',
+                    recipients: ['team@bodai.co'],
+                    isHTML: false,
+                  );
+
+                  await FlutterEmailSender.send(email);
+
+                  const snackBar = SnackBar(
+                      content: Text(
+                          'We\'ve received your request, will be in touch shortly to confirm account deletion.'));
+                  scaffoldMessenger.removeCurrentSnackBar();
+                  scaffoldMessenger.showSnackBar(snackBar);
+                }
+
+                navigator.pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
