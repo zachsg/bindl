574dde4c19dd3339761c39f35d464cec7825b118
zachsg <zach@bindl.co>
2022-06-17 21:46:39 -0700

    cleanup authentication view ux

--- lib/features/auth/auth_view.dart
+++ lib/features/auth/auth_view.dart
@@ -1,10 +1,13 @@
 import 'package:bodai/features/auth/auth_state.dart';
+import 'package:bodai/providers/providers.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'auth_controller.dart';
 import 'widgets/email_text_field_widget.dart';
+
+final requestedMagicLinkProvider = StateProvider<bool>((ref) => false);
 
 class AuthView extends StatefulWidget {
   const AuthView({super.key});
@@ -31,15 +34,36 @@ class AuthViewState extends AuthState<AuthView> {
               width: MediaQuery.of(context).size.width * 0.7,
             ),
           ),
-          const EmailTextFieldWidget(),
           Consumer(
-            builder: (context, ref, child) => ElevatedButton(
-              onPressed: () async {
-                final email = ref.read(emailAuthProvider);
-                await AuthController.signIn(email, kIsWeb);
-              },
-              child: const Text('Get Magic Sign-In Link'),
-            ),
+            builder: (context, ref, child) =>
+                ref.watch(requestedMagicLinkProvider)
+                    ? const SizedBox()
+                    : const EmailTextFieldWidget(),
+          ),
+          Consumer(
+            builder: (context, ref, child) =>
+                ref.watch(requestedMagicLinkProvider)
+                    ? Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Check your email (${ref.watch(emailAuthProvider)}) for a link to sign into Bodai!',
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    : ref.watch(loadingProvider)
+                        ? const CircularProgressIndicator()
+                        : ElevatedButton(
+                            onPressed: () async {
+                              ref.read(loadingProvider.notifier).state = true;
+                              final email = ref.read(emailAuthProvider);
+                              await AuthController.signIn(email, kIsWeb);
+                              ref
+                                  .read(requestedMagicLinkProvider.notifier)
+                                  .state = true;
+                              ref.read(loadingProvider.notifier).state = false;
+                            },
+                            child: const Text('Get Magic Sign-In Link'),
+                          ),
           ),
         ],
       ),
